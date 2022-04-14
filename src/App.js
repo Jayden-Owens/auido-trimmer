586373294300de3b6a3ec9af5fd5398d0dff3ef2
Jayden Owens <77424127+Jayden-Owens@users.noreply.github.com>
2022-04-13 22:42:15 -0600

    trimmer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import EditPage from './pages/EditPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<Router>
+			<Switch>
+				<Route path='/' component={HomePage} exact />
+				<Route path='/edit' component={EditPage} exact />
+			</Switch>
+		</Router>
+	);
+};
 
 export default App;
